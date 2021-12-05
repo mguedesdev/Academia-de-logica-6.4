da448cafa644f1dec58cf07c7badbdf55f831267
mathheusguedes <mathheus.gr@gmail.com>
2021-12-05 16:06:30 -0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Academia-de-logica-6.4
+
+
+# Update no nosso forms com FlexBox
+
+  Boas vindas ao repositório de lógica de programação, desenvolvemos essa aplicação no intuito de auxiliar na fixação do conteúdo do bloco 6
+
+![enter image description here](https://i.ibb.co/qsyMMSx/Captura-de-tela-de-2021-12-05-15-39-35.png)
+
+## Como vai funcionar
+
+Você deve dar um upgrade no forms passado na ultima mentoria de lógica, lembre-se de utilizar flexbox.
+A imagem acima serve como referencia de como a aplicação deve ficar.
+Somente o arquivo CSS deve ser alterado.
+
+## O que já vem pronto
+* Todo o forms;
+* Todo o javaScript
---
+++ script.js
@@ -0,0 +1,60 @@
+const checkin = document.querySelector('.inputCheckin');  
+const checkout = document.querySelector('.inputCheckout'); 
+const bedroom = document.querySelectorAll('.bedroom'); 
+const select = document.querySelector('.nPessoas'); 
+const textArea = document.querySelector('.obs'); 
+const btnSubmit = document.querySelector('#submit-btn');
+const btnClear = document.querySelector('#clear-btn');
+const ol = document.querySelector('ol');
+
+function getQuarto(){
+  for (let i = 0; i <bedroom.length; i++) {
+    if (bedroom[i].checked) {
+      return bedroom[i].value;
+    }
+  }
+}
+
+function getPessoas(){
+  return select.options[select.selectedIndex].value;
+}
+
+function getData(value){
+  data = new Date(value)
+  dataFormatada = ((data.getDate() )) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear(); 
+  return dataFormatada;
+}
+
+function addItemList(e){
+  e.preventDefault()
+  const li = document.createElement('li');
+  const pQuarto = document.createElement('p');
+  const pObs = document.createElement('p');
+  li.innerText = 'Reserva para o dia ' + getData(checkin.value) + ' até o dia ' + getData(checkout.value);
+  pQuarto.innerText = 'Quarto ' + getQuarto() + ' - Para ' + getPessoas() + ' pessoas ';
+  pObs.innerText = 'Obs: ' + textArea.value;
+  li.appendChild(pQuarto);
+  li.appendChild(pObs);
+  ol.appendChild(li);
+  saveLocalStorage();
+}
+btnSubmit.addEventListener('click', addItemList)
+
+function clearOl(){
+  ol.innerHTML = '';
+  saveLocalStorage();
+}
+btnClear.addEventListener('click', clearOl);
+
+function saveLocalStorage(){
+  localStorage.setItem('lista', ol.innerHTML)
+}
+
+function loadLocalStorage(){
+  const load = localStorage.getItem('lista');
+  if (load) {
+    ol.innerHTML = load;
+  }
+}
+
+window.onload = loadLocalStorage;
